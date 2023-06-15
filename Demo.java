22dc4e7792964f818e45b339ee3846058bdd25c0
Lon Lrn <lonlrn@DESKTOP-UNQVNT2.2e.lan>
2023-06-15 13:38:12 +0200

    EA

--- Demo.java
+++ Demo.java
@@ -1,28 +1,8 @@
-class Demo{   // DN
+class Demo{   // EA
 	public static void main(String[] args){
 		System.out.println("Beginnen");
-		Fiets f = new Fiets(17);
-		Fiets f2 = new Fiets();
-		System.out.println(">> "+f2.snelheid);
-	}
-
-}
-class Voertuig{
-	Voertuig(){
-		System.out.println("dit zien we niet");	
-	}
-}
-class Fiets extends Voertuig{
-	int snelheid = 25;
-	Fiets(){
-		this(8);
-		snelheid = 9;
-	}
-	Fiets(long w){
-		System.out.println("de andere");
-	}
-	Fiets(Integer i){
-		System.out.println("nog een??");
+		int[] getallen = new int[14];
+		System.out.println(getallen);
 	}
 
 }

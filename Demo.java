018b5ccdccab1361277a092f57de58caabc524a3
Lon Lrn <lonlrn@DESKTOP-UNQVNT2.2e.lan>
2023-06-15 13:45:45 +0200

    EI

--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
-class Demo{   // EH
+class Demo{   // EI
 	public static void main(String[] args){
 		System.out.println("Beginnen");
 		int[] getallen[] = {{4, 5, 6, 7},{ 2, 3, 4, 5 },null};
-		System.out.println(getallen[2]);
+		System.out.println(getallen[2].length);
 	}
 
 }

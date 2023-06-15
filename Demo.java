eade1a8a7a61430502687d1902b5c69656bf0530
Lon Lrn <lonlrn@DESKTOP-UNQVNT2.2e.lan>
2023-06-15 13:47:40 +0200

    EJ

--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
-class Demo{   // EI
+class Demo{   // EJ
 	public static void main(String[] args){
 		System.out.println("Beginnen");
-		int[] getallen[] = {{4, 5, 6, 7},{ 2, 3, 4, 5 },null};
-		System.out.println(getallen[2].length);
+		boolean[] bbb = new boolean[5];
+		System.out.println(bbb[2]);
 	}
 
 }

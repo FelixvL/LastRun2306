e780fd8ef7fe7db7e812508a8055c1ddbb113054
Lon Lrn <lonlrn@DESKTOP-UNQVNT2.2e.lan>
2023-06-15 13:24:50 +0200

    DH

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
-class Demo{   // DG
+class Demo{   // DH
 	public static void main(String[] args){
 		System.out.println("Beginnen");
-		Fiets f = new Fiets();
+		Fiets f = new Fiets(17);
 		Fiets f2 = new Fiets();
 		System.out.println(">> "+f2.snelheid);
 	}
@@ -11,8 +11,11 @@ class Demo{   // DG
 class Fiets{
 	static int snelheid = 25;
 	Fiets(int s){
-		System.out.println("Fietsje erbij");
+		System.out.println("Fietsje erbij: "+ s);
 		snelheid = s;
+	}
+	Fiets(){
+		snelheid = 9;
 	}
 
 }

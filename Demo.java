9459026e38b00f51b426b65af3e9814843b076c2
Lon Lrn <lonlrn@DESKTOP-UNQVNT2.2e.lan>
2023-06-15 13:51:17 +0200

    EL

--- Demo.java
+++ Demo.java
@@ -1,10 +1,20 @@
-class Demo{   // EK
+class Demo{   // EL
 	public static void main(String[] args){
 		System.out.println("Beginnen");
-		Hond[] bbb = new Hond[5];
-		System.out.println(bbb[2]);
+		Hond h = new Hond();
+		h.blaffen();
+		iets(h);
+		h.blaffen();
+
+	}
+	static void iets(Hond h){
+		h.ap = 33;
 	}
 
 }
 class Hond{
+	int ap = 24;
+	void blaffen(){
+		System.out.println(ap);
+	}
 }

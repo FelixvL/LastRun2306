f165774cb66803a2286481c534fcf1475f73da1c
Lon Lrn <lonlrn@DESKTOP-UNQVNT2.2e.lan>
2023-06-15 13:44:18 +0200

    EH

--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
-class Demo{   // EF
+class Demo{   // EH
 	public static void main(String[] args){
 		System.out.println("Beginnen");
-		int[] getallen[] = {{},{ 2, 3, 4, 5 }};
-		System.out.println(getallen[0].length);
+		int[] getallen[] = {{4, 5, 6, 7},{ 2, 3, 4, 5 },null};
+		System.out.println(getallen[2]);
 	}
 
 }

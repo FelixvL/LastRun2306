03e654fe06038237b3a6a4cfcd36f4739b0eea94
Lon Lrn <lonlrn@DESKTOP-UNQVNT2.2e.lan>
2023-06-15 13:19:20 +0200

    DE

--- Demo.java
+++ Demo.java
@@ -1,19 +1,18 @@
-class Demo{   // DD
+class Demo{   // DE
 	public static void main(String[] args){
 		System.out.println("Beginnen");
 		Fiets f = new Fiets();
-		f.doen(6);
-		String iets = new String("voordoen");
-		iets = iets.concat("nadoen");
-		System.out.println(iets);
+		Fiets f2 = new Fiets();
+		f.snelheid = 14;
+		System.out.println(">> "+f2.snelheid);
 	}
 
 }
 
 class Fiets{
-	int waarde = 25;
-	int doen(int waardevol){
-		System.out.println(waarde);
-		return 37;
+	int snelheid = 25;
+	Fiets(){
+		System.out.println("Fietsje erbij");
 	}
+
 }

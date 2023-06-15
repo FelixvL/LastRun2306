f7851fa9904c55a14c2a7a6213704ef7ed7a4104
Lon Lrn <lonlrn@DESKTOP-UNQVNT2.2e.lan>
2023-06-15 13:13:44 +0200

    DC

--- Demo.java
+++ Demo.java
@@ -1,15 +1,19 @@
-class Demo{   // DB
+class Demo{   // DC
 	public static void main(String[] args){
 		System.out.println("Beginnen");
 		Fiets f = new Fiets();
 		f.doen(6);
+		String iets = new String("voordoen");
+		iets.concat("nadoen");
+		System.out.println(iets);
 	}
 
 }
 
 class Fiets{
 	int waarde = 25;
-	void doen(int waarde){
+	int doen(int waardevol){
 		System.out.println(waarde);
+		return 37;
 	}
 }
